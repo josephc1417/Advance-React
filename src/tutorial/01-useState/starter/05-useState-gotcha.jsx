c8cfba691177fbdf2ce015f891b4e892519830a9
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-04 04:54:08 -0400

    useState

---
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+
+
+
+const UseStateGotcha = () => {
+  const [count, setCount] = useState(0);
+  
+  const handleClick =() => {
+    setCount(count + 1);
+    console.log(count);
+  }
+  return(
+    <div>
+      <h2>{count}</h2>
+      <button type='button'  className='btn' onClick={handleClick}>Add More</button>
+    </div>
+  ) 
+};
+
+export default UseStateGotcha;
